cd4f6fab2cb0f44a5987ad44c0c519a3b398ef74
GauravP23 <142415456+GauravP23@users.noreply.github.com>
2023-10-14 10:56:00 +0530

    app.js updated

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ let machineVal;
 
 let btn1 = document.querySelector(".btn-1");
 
-btn1.addEventListener("click",()=>{
-    document.getElementById("box").style.display = "flex";
+btn1.addEventListener("click", () => {
+  document.getElementById("box").style.display = "flex";
 
-    maxNum = document.getElementById("max").value;
-    machineVal = Math.floor(Math.random() * maxNum) + 1;
-    console.log("I Am Giving You Answer Of This Which is: " + machineVal);
-    console.log("But This Not Fair In Games. Enjoy Your Life :) ");
-    console.log("Let's Connect:  https://www.linkedin.com/in/ajaymahiwal/");
-  
-    document.getElementById("msg").innerHTML =
-      "The Game Has Started. Guess Number From 1 to " + maxNum;
+  maxNum = document.getElementById("max").value;
+  machineVal = Math.floor(Math.random() * maxNum) + 1;
+  console.log("I Am Giving You Answer Of This Which is: " + machineVal);
+  console.log("But This Not Fair In Games. Enjoy Your Life :) ");
+  console.log("Let's Connect:  https://www.linkedin.com/in/ajaymahiwal/");
+
+  document.getElementById("msg").innerHTML =
+    "The Game Has Started. Guess Number From 1 to " + maxNum;
 });
 
 // function startGame() {
@@ -24,7 +24,6 @@ btn1.addEventListener("click",()=>{
 // function show() {
 // }
 
-
 function guessGame() {
   userN = document.getElementById("userVal").value;
   let finalRes = gameLogic(userN);
@@ -33,18 +32,18 @@ function guessGame() {
 
 function gameLogic() {
   while (true) {
-  //   if (userN == "quit") {
-  //     return "Quitting.............";
-  //     break;
-  //   }
+    //   if (userN == "quit") {
+    //     return "Quitting.............";
+    //     break;
+    //   }
     if (userN == machineVal) {
       return "You Guess Is correct.You Won.";
       break;
     } else {
       if (userN < machineVal) {
-        return "Ooops, Wrong Guess. Re-Enter ---Hint: Smaller number then Random number.";
-      } else {
         return "Ooops, Wrong Guess. Re-Enter ---Hint: Higher number then Random number.";
+      } else {
+        return "Ooops, Wrong Guess. Re-Enter ---Hint: Smaller number then Random number.";
       }
     }
   }
